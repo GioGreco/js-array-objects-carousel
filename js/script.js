0daf41d0756d4a09b70bb92268b0a6a51eb09fd6
GioGreco <grecogiorgio.97@gmail.com>
2022-10-22 18:44:10 +0200

    add pause / reverse buttons + mediaqueries

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 "use strict";
-
 const images = [
     {
         url: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg',
@@ -29,44 +28,18 @@ const images = [
         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
     },
 ];
-
-// for(let landscape of images){
-//     createCol(landscape);
-// }
-
-// function createCol(member){
-//     const showHTML = document.getElementById('show');
-//     const col = document.createElement('div');
-//     col.className = 'landscape-wrapper d-none';
-
-//     const colContent = `
-//     <img class="landscape" src="${member.url}" alt="${member.title} landscape">
-//     <div class="caption position-absolute">
-//         <h2 class="country">${member.title}</h2>
-//         <h4 class="description">${member.description}</h4>
-//     </div>
-//     `;
-
-//     col.innerHTML += colContent;
-
-//     showHTML.append(col);
-
-//     const slideshow = setInterval(toggleShow, 3000);
-
-//     const toggleShow = 
-
-// }
-
+//display all images in HTML, all of them have d-none;
 const displayImages = function(item, index){
     const showHTML = document.getElementById('show');
     const col = document.createElement('div');
     col.className = 'landscape-wrapper d-none';
 
     const colContent = `
-    <img class="landscape" src="${item.url}" alt="${item.title} landscape">
+    <img class="landscape fade" src="${item.url}" alt="${item.title} landscape">
     <div class="caption position-absolute">
         <h2 class="country">${item.title}</h2>
         <h4 class="description">${item.description}</h4>
+        <span class='drag-down'><i class="fa-solid fa-caret-down"></i></span>
     </div>
     `;
 
@@ -75,8 +48,6 @@ const displayImages = function(item, index){
     showHTML.append(col);
 
     const miniaturesHTML = document.querySelector('.miniatures');
-    // const miniatureHTML = document.createElement('div');
-    // miniatureHTML.className = 'miniature';
 
     const minContent = `
     <div class="miniature micro" style="background-image: url(${item.url}); background-size: cover;"></div>
@@ -87,24 +58,51 @@ const displayImages = function(item, index){
 
 images.forEach(displayImages);
 
+//toggle d-none when a specific condition is met
 function displayLand(){
+
     let activeLand = 0;
+    let counter = 5000;
     let landscapesHTML = document.querySelectorAll('.landscape-wrapper');
     let microsHTML = document.querySelectorAll('.miniature');
+    let repeat;
+    let forward = true;
+
+    //show first landscape + miniature;
     landscapesHTML[activeLand].classList.toggle('d-none');
     microsHTML[activeLand].classList.toggle('micro');
-    const slideshow = setInterval(function(){
-        landscapesHTML[activeLand].classList.toggle('d-none');
-        microsHTML[activeLand].classList.toggle('micro');
-        activeLand++;
-        if(activeLand == landscapesHTML.length){
-            activeLand = 0;
+
+    //auto-play slideshow: interval function, forwards or backwards.
+    const slideshow = () => {
+        if(forward){
+            repeat = setInterval(function(){
+                landscapesHTML[activeLand].classList.toggle('d-none');
+                microsHTML[activeLand].classList.toggle('micro');
+                activeLand++;
+                if(activeLand == landscapesHTML.length){
+                    activeLand = 0;
+                }
+                landscapesHTML[activeLand].classList.toggle('d-none');
+                microsHTML[activeLand].classList.toggle('micro');
+            }, counter);
         }
-        landscapesHTML[activeLand].classList.toggle('d-none');
-        microsHTML[activeLand].classList.toggle('micro');
-    }, 5000);
+        else if(!forward){
+            repeat = setInterval(function(){
+                landscapesHTML[activeLand].classList.toggle('d-none');
+                microsHTML[activeLand].classList.toggle('micro');
+                activeLand--;
+                if(activeLand < 0){
+                    activeLand = landscapesHTML.length - 1;
+                }
+                landscapesHTML[activeLand].classList.toggle('d-none');
+                microsHTML[activeLand].classList.toggle('micro');
+            }, counter);
+        }
 
+    }
+    slideshow();
 
+    //toLeft landscape button
     const btnLeft = document.querySelector('.toggle-land.toLeft');
     btnLeft.addEventListener('click', function(){
         landscapesHTML[activeLand].classList.toggle('d-none');
@@ -115,8 +113,8 @@ function displayLand(){
         }
         landscapesHTML[activeLand].classList.toggle('d-none');
         microsHTML[activeLand].classList.toggle('micro');
-    })
-
+    });
+    //toRight landscape button
     const btnRight = document.querySelector('.toggle-land.toRight');
     btnRight.addEventListener('click', function(){
         landscapesHTML[activeLand].classList.toggle('d-none');
@@ -129,6 +127,43 @@ function displayLand(){
         microsHTML[activeLand].classList.toggle('micro');
     })
 
+    //pause/resume auto-play slideshow
+    const pausePlayHTML = document.querySelector('.pausePlay');
+    let status = 0;
+    pausePlayHTML.addEventListener('click', function(){
+        const landscapeHTML = document.querySelectorAll('.landscape');
+        console.log(landscapeHTML);
+        landscapeHTML.forEach((item) => item.classList.toggle('fade'));
+        pausePlayHTML.classList.toggle('play');
+        pausePlayHTML.classList.toggle('pause');
+        (pausePlayHTML.classList.contains('play'))? pausePlayHTML.innerHTML = `<i class="fa-solid fa-play"></i>` : pausePlayHTML.innerHTML = `<i class="fa-solid fa-pause"></i>`;
+        
+        if(status == 0){
+            clearInterval(repeat);
+            status = 1;
+            console.log(status);
+        }
+        else if(status == 1){
+            slideshow();
+            status = 0;
+            console.log(status);
+        }
+    });
+    //invert slideshow
+    const reverseHTML = document.querySelector('.reverse');
+    reverseHTML.addEventListener('click', function(){
+        if(forward){
+            forward = false;
+            clearInterval(repeat);
+            reverseHTML.classList.toggle('action');
+        }
+        else if(!forward){
+            forward = true;
+            clearInterval(repeat);
+            reverseHTML.classList.toggle('action');
+        }
+        slideshow();
+    });
 }
 
 displayLand();
